214293338ac12b8414a5944d6803fa4809d8f83f
ryan mckennon <ryan.mckennon@gmail.com>
2017-01-24 15:39:18 -0500

    Add documentation tool

---
+++ Housing.Data/Housing.Data.Client/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Housing.Data.Client.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
