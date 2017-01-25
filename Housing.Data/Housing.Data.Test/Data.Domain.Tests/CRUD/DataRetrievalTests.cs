78041285a42c0d62a4dae54c103289153ee79cc4
Matthew O'Brien <ohbrien93@hotmail.com>
2017-01-25 14:18:00 -0500

    minor commit

---
+++ Housing.Data/Housing.Data.Test/Data.Domain.Tests/CRUD/DataRetrievalTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Housing.Data.Test.Data.Domain.Tests.CRUD
+{
+  
+  public class DataRetrievalTests
+  {
+     
+    [Fact]
+    public void GetAssociates()
+    {
+
+    }
+
+  }
+}
