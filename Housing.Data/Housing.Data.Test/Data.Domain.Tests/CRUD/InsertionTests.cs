4fb55459b48f503b29fbcaffdcf3fead6ec7b2c5
greg mckinney <gmck95@msn.com>
2017-02-21 10:25:25 -0500

    trying to get insert gender to work

--- Housing.Data/Housing.Data.Test/Data.Domain.Tests/CRUD/InsertionTests.cs
+++ Housing.Data/Housing.Data.Test/Data.Domain.Tests/CRUD/InsertionTests.cs
@@ -18,18 +18,77 @@ namespace Housing.Data.Test.Data.Domain.Tests.CRUD
     {
         private Housing.Data.Domain.AccessMapper map = new Housing.Data.Domain.AccessMapper();
         private Housing.Data.Domain.CRUD.AccessHelper ah = new Housing.Data.Domain.CRUD.AccessHelper();
+        public GenderDao gender = new GenderDao()
+        {            
+            Name = "Test"            
+        };
+
+        Batch batch = new Batch()
+        {
+            BatchId = 1,
+            Name = "Dot net",
+            Instructor = "Fred",
+            Technology = "C#",
+            StartDate = DateTime.Parse("03/03/2017"),
+            EndDate = DateTime.Parse("06/15/2017"),
+            Active = true
+        };
+
+        Associate associate = new Associate()
+        {
+            FirstName = "First",
+            LastName = "Last",
+            GenderId = 1,
+            BatchId = 1,
+            PhoneNumber = "7321231233",
+            Email = "come@me.net",
+            HasCar = true,
+            HasKeys = true
+        };
+
+        HousingComplex hc = new HousingComplex()
+        {
+            HousingComplexId = 1,
+            Name = "The Townes",
+            PhoneNumber = "555-555-5555",
+            Address = "506 Pride Ave",
+            Active = true
+        };
+
+        HousingUnit hu = new HousingUnit()
+        {
+            HousingUnitId = 1,
+            HousingUnitName = "test",
+            HousingComplexId = 1,
+            AptNumber = "600",
+            GenderId = 1,
+            MaxCapacity = 6,
+            LeaseEndDate = DateTime.Parse("06/30/2017"),
+            Active = true
+        };
+
+        HousingData hd = new HousingData()
+        {
+            HousingDataId = 1,
+            HousingDataAltId = "The Townes test",
+            HousingUnitId = 1,
+            AssociateId = 1,
+            MoveInDate = DateTime.Parse("02/28/2017"),
+            MoveOutDate = DateTime.Parse("06/30/2017"),
+            Active = true
+        };
+
 
         [Fact]
         public void Test_InsertGender()
         {
             var mock = new Mock<IEF>();
-
-            var itemsInserted = new List<Gender>();
-
+            
             mock
-                .Setup(i => i.InsertGender(It.IsAny<Gender>()))
-                .Callback((Gender item) => itemsInserted.Add(item));
-            Assert.NotNull(itemsInserted);
+                .Setup(i => i.InsertGender(It.IsAny<Gender>())).Returns(true);
+            AccessHelper ah = new AccessHelper(mock.Object);
+            var g = ah.InsertGender(gender);
+            Assert.True(g);
             mock.Verify(m => m.InsertGender(It.IsAny<Gender>()), Times.Once());
         }
 
@@ -50,10 +109,11 @@ namespace Housing.Data.Test.Data.Domain.Tests.CRUD
         [Fact]
         public void Test_InsertAssociate()
         {
+            
             var mock = new Mock<IEF>();
 
             var itemsInserted = new List<Associate>();
-
+            itemsInserted.Add(associate);
             mock
                 .Setup(i => i.InsertAssociate(It.IsAny<Associate>()))
                 .Callback((Associate item) => itemsInserted.Add(item));
